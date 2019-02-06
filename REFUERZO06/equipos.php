58160b3584c670519862a8cab69c7600aa9d6061
riluas <44229797+riluas@users.noreply.github.com>
2019-02-06 12:19:39 +0100

    Add files via upload

---
+++ REFUERZO06/equipos.php
@@ -0,0 +1,36 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "liga");
+if ($conexion->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+}else{
+  $resultado = $conexion->query("SELECT * FROM equipo");
+}
+ ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="equipos.php">Equipos</a>
+      <a href="jugadores.php">Jugadores</a>
+    </nav>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>Nombre</td>
+        <td>Ciudad</td>
+      </tr>
+      <?php
+        foreach ($resultado as $equipo) {
+          echo "<tr>";
+          echo "<td>".$equipo['nombre']."</td>";
+          echo "<td>".$equipo['ciudad']."</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
